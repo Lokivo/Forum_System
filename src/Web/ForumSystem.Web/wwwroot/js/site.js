980bbbebf0c60d927c47f6d5484a04ce9d8fff21
LokIvo <ivailostoyanov95@gmail.com>
2020-10-07 17:14:49 +0300

    Implemented local time, and recursive comments

--- src/Web/ForumSystem.Web/wwwroot/js/site.js
+++ src/Web/ForumSystem.Web/wwwroot/js/site.js
@@ -1,4 +1,15 @@
 ﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
 // for details on configuring this project to bundle and minify static web assets.
+$(function () {
+    $('time').each(function (i, e) {
+        const dateTimeValue = $(e).attr("datetime");
+        if (!dateTimeValue) {
+            return;
+        }
 
+        const time = moment.utc(dateTimeValue).local();
+        $(e).html(time.format("llll"));
+        $(e).attr("title", $(e).attr("datetime"));
+    });
+});
 // Write your JavaScript code.
